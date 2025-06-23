77ffb8ae2715e0d26556babb3ad30beadb4bd7e3
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-23 17:35:28 +0900

    [FEAT] 실행 시간 측정 Aspect 구현

--- src/main/java/com/github/kyungmin08g/zephyro/test/service/TestService.java
+++ src/main/java/com/github/kyungmin08g/zephyro/test/service/TestService.java
@@ -1,11 +1,18 @@
 package com.github.kyungmin08g.zephyro.test.service;
 
+import com.github.kyungmin08g.zephyro.annotation.ExecutionTimer;
 import org.springframework.stereotype.Service;
 
 @Service
 public class TestService {
 
+  @ExecutionTimer
   public void print() {
-    System.out.println("Hello World!");
+    try {
+      Thread.sleep(4000);
+      System.out.println("Hello World!");
+    } catch (InterruptedException e) {
+      e.printStackTrace();
+    }
   }
 }
