2433d6141eb90b6bb8ed626f90f47b8421e044bb
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-23 12:04:40 +0900

    [CHORE] 프로젝트 초기 세팅

---
+++ src/test/java/com/github/kyungmin08g/zephyro/ZephyroApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.kyungmin08g.zephyro;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZephyroApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
