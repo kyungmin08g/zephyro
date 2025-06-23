12c15d7fb7506f9bc9e2a003c93677d51ea4c97f
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-24 08:51:32 +0900

    [CHORE] 레벨 색상 정의

---
+++ src/main/java/com/github/kyungmin08g/zephyro/core/utils/enums/LevelColor.java
@@ -0,0 +1,19 @@
+package com.github.kyungmin08g.zephyro.core.utils.enums;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+
+@Getter
+@AllArgsConstructor
+public enum LevelColor {
+
+  RED("\\u001B[31m"),
+  YELLOW("\\u001B[33m"),
+  GREEN("\\u001B[32m"),
+  BLUE("\\u001B[34m"),
+  MAGENTA("\\u001B[35m"),
+  CYAN("\\u001B[36m"),
+  WHITE("\\u001B[37m");
+
+  private final String color;
+}
