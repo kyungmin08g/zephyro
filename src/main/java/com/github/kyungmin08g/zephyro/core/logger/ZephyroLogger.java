0c2f77d17df7ed3d30a8e8246619237dc072cbb4
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-23 22:25:03 +0900

    [FEAT] 커스텀 Logger 클래스 일부 구현

---
+++ src/main/java/com/github/kyungmin08g/zephyro/core/logger/ZephyroLogger.java
@@ -0,0 +1,44 @@
+package com.github.kyungmin08g.zephyro.core.logger;
+
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.LinkedBlockingQueue;
+
+@Component
+public class ZephyroLogger {
+  private final BlockingQueue<String> buffer =  new LinkedBlockingQueue<>(1000);
+  private final Thread thread;
+
+  public ZephyroLogger() {
+    this.thread = new Thread(() -> {
+      try {
+        while (!Thread.currentThread().isInterrupted()) {
+          String log = buffer.take();
+          System.out.println(log);
+        }
+      } catch (InterruptedException e) {
+        Thread.currentThread().interrupt();
+        e.fillInStackTrace();
+      }
+    });
+
+    this.thread.setDaemon(true);
+    this.thread.start();
+  }
+
+  public void info(String message) {
+    boolean offered = buffer.offer(message);
+    if (!offered) {
+      flushBuffer();
+      buffer.offer(message);
+    }
+  }
+
+  private synchronized void flushBuffer() {
+    String log;
+    while ((log = buffer.poll()) != null) {
+      System.out.println(log);
+    }
+  }
+}
