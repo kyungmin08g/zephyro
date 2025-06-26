389f9ee14574b169992175a5aa5f6ab5306a9c3f
Kim KyungMin <k.kyungmin2892@gmail.com>
2025-06-26 18:55:37 +0900

    [UPDATE] QueryLogConfig 인터셉터 등록 수정 (#7)

--- src/main/java/com/github/kyungmin08g/zephyro/core/config/QueryLogConfig.java
+++ src/main/java/com/github/kyungmin08g/zephyro/core/config/QueryLogConfig.java
@@ -1,14 +1,21 @@
 package com.github.kyungmin08g.zephyro.core.config;
 
 import com.github.kyungmin08g.zephyro.query.interceptor.QueryLogInterceptor;
+import lombok.RequiredArgsConstructor;
+import org.hibernate.cfg.AvailableSettings;
+import org.springframework.boot.autoconfigure.orm.jpa.HibernatePropertiesCustomizer;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@RequiredArgsConstructor
 public class QueryLogConfig {
+  private final QueryLogInterceptor queryLogInterceptor;
 
   @Bean
-  public QueryLogInterceptor getQueryLogInterceptor() {
-    return new QueryLogInterceptor();
+  public HibernatePropertiesCustomizer getHibernatePropertiesCustomizer() {
+    return hibernateProperties -> {
+      hibernateProperties.put(AvailableSettings.STATEMENT_INSPECTOR, queryLogInterceptor);
+    };
   }
 }
